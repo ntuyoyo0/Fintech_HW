5054985020928048284f309acd1a573b65334d94
leo08260826 <leo082060826@gmail.com>
2019-04-10 16:50:57 +0800

    hw2

---
+++ hw2/stock_anay_main.py
@@ -0,0 +1,51 @@
+from stock_crawler import crawler
+import jieba
+import pandas as pd
+
+def num(string):
+	if string == "down":
+		return 1
+	if string == "up":
+		return 0
+
+stocks1 = ['台泥','亞泥','統一','台塑','南亞','台化','遠東新','中鋼','正新','和泰車']
+stocks2 = ['光寶科','聯電','台達電','鴻海','國巨','台積電','華碩','廣達','研華','南亞科']
+stocks3 = ['友達','中華電','聯發科','可成','台灣高鐵','彰銀','中壽','華南金','富邦金','國泰金']
+stocks4 = ['開發金','玉山金','元大金','兆豐金','台新金','永豐金','中信金','第一金','統一超','大立光']
+stocks5 = ['台灣大','群創','日月光投控','遠傳','和碩','中租-KY','上海商銀','合庫金','台塑化','寶成']
+
+stocks = stocks1 + stocks2 + stocks3 + stocks4 + stocks5
+
+list_up = []
+list_mid = []
+list_down = []
+
+fp_up = open("up.txt","r",encoding = 'utf8')
+fp_mid = open("mid.txt","r",encoding = 'utf8')
+fp_down = open("down.txt","r",encoding = 'utf8')
+
+for line in fp_up.readlines():
+	list_up.append(line.strip('\n'))
+fp_up.close()
+for line in fp_mid.readlines():
+	list_mid.append(line.strip('\n'))
+fp_mid.close()
+for line in fp_down.readlines():
+	list_down.append(line.strip('\n'))
+fp_down.close()
+
+test2 = ['1/1/2019','2/1/2019','3/1/2019']
+test3 = ['1/31/2019','2/28/2019','3/31/2019']
+
+dictionary = {}
+for item in stocks1:
+	col = []
+	for i in range(3):
+		col.append(num(crawler(item,test2[i],test3[i],list_up,list_mid,list_down)))
+	dictionary.update({item:col})
+
+df = pd.DataFrame(dictionary)
+coocc = df.T.dot(df)
+print(df)
+print(coocc)
+coocc.to_csv("./out.csv",encoding="utf_8")
---
+++ hw2/stock_crawler.py
@@ -0,0 +1,88 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+from NER import ner
+
+def crawler(name,start_date,end_date,list_up,list_mid,list_down):
+	browser = webdriver.Chrome('./chromedriver.exe')
+	url = "https://www.google.com/"
+	browser.get(url)
+
+	search = browser.find_element_by_name('q')
+	search.send_keys(name)
+	search.send_keys(Keys.RETURN)
+	time.sleep(3)
+
+	search = browser.find_elements_by_css_selector("a")
+	for item in search:
+		if item.text == "新聞":
+			item.click()
+			break
+	time.sleep(3)
+
+	advances = browser.find_elements_by_css_selector("a")
+	for item in advances:
+		if item.text == "工具":
+			advance = item
+			break
+	advance.click()
+	time.sleep(1)
+
+	arounds = browser.find_elements_by_class_name("hdtb-mn-hd")
+	for item in arounds:
+		if item.text == "最近":
+			around = item
+			break
+	around.click()
+	time.sleep(1)
+
+	customizes = browser.find_elements_by_class_name("q")
+	for item in customizes:
+		if item.text == "自訂日期範圍...":
+			customize = item
+			break
+	customize.click()
+	time.sleep(1)
+
+	start = browser.find_element_by_id("cdr_min")
+	end = browser.find_element_by_id("cdr_max")
+	search_date = browser.find_element_by_class_name("cdr_go")
+	start.send_keys(start_date)
+	end.send_keys(end_date)	
+	end.send_keys(Keys.ENTER)
+	# search_date.submit()
+	time.sleep(3)
+
+	search = browser.find_elements_by_class_name("g")
+	#print(search[0].get_attribute('innerHTML'))
+
+	flag = 0
+	flag2 = 0
+	for item in search:
+		news = item.find_element_by_css_selector("h3 a")
+		print(news.text)
+		#print(news.get_attribute('href'))
+		output = ner(news.text,news.get_attribute('href'),list_up,list_mid,list_down)
+		print(output)
+		if output == "down":
+			flag = flag-1
+			flag2 = 1
+		elif output == "up":
+			flag = flag+1
+
+		print("================")
+
+	browser.quit()
+
+	# if flag<0:
+	# 	return "down"
+	# elif flag>=0:
+	# 	return "up"
+
+	if flag2 == 1:
+		return "down"
+	else:
+		return "up"
+	
+
+
