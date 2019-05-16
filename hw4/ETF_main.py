040fa135d91025a43a9079f632762b3a718ec093
leo08260826 <leo082060826@gmail.com>
2019-05-16 21:20:23 +0800

    modify B rank

--- hw4/ETF_main.py
+++ hw4/ETF_main.py
@@ -6,7 +6,7 @@ Yahoo_ETFs_2 = ['HTUS','FLAG','FTLS','FMF','QAI','MNA','CPI','QMN','MCRO','QLS',
 Yahoo_ETFs_3 = ['BTAL','MOM','DIVA','SIZ','CHEP','XVZ','HDG','RALS','ALTS','MRGR','SVXY','UVXY','VIXY','VIXM','DIVY']
 Yahoo_ETFs = Yahoo_ETFs_1 + Yahoo_ETFs_2 + Yahoo_ETFs_3
 dfs = []
-INTERVAL = 30
+INTERVAL = 7
 
 # crawler on Yahoo
 # for i in Yahoo_ETFs:
@@ -58,39 +58,38 @@ elif INTERVAL == 7:
 
 # print(excess_return.head(10))
 
-# # A
-# mu = calculate.mu_(excess_return)
-# sigma = calculate.sigma_(excess_return)
-# skew = calculate.skew_(excess_return)
-# assr = calculate.assr_(mu,sigma,no_risk,skew)
-# A_dict = dict(zip(result.columns,assr))
-# A_rank =  (calculate.sort_by_value(A_dict))
-# A_rank = A_rank[::-1]
-# A_last_dict = {}
-# A_last_dict["A"]=A_rank
-# A_last = pd.DataFrame.from_dict(A_last_dict)
+# A
+mu = calculate.mu_(excess_return)
+sigma = calculate.sigma_(excess_return)
+skew = calculate.skew_(excess_return)
+assr = calculate.assr_(mu,sigma,no_risk,skew)
+A_dict = dict(zip(result.columns,assr))
+A_rank =  (calculate.sort_by_value(A_dict))
+A_rank = A_rank[::-1]
+A_last_dict = {}
+A_last_dict["A"]=A_rank
+A_last = pd.DataFrame.from_dict(A_last_dict)
 
-# # B
-# omega = calculate.omega_(with_risk,no_risk)
-# B_dict = dict(zip(result.columns,omega))
-# B_rank =  (calculate.sort_by_value(B_dict))
-# B_rank = B_rank[::-1]
-# B_last_dict = {}
-# B_last_dict["B"]=B_rank
-# B_last = pd.DataFrame.from_dict(B_last_dict)
+# B
+omega = calculate.omega_(with_risk,no_risk)
+B_dict = dict(zip(result.columns,omega))
+B_rank =  (calculate.sort_by_value(B_dict))
+B_last_dict = {}
+B_last_dict["B"]=B_rank
+B_last = pd.DataFrame.from_dict(B_last_dict)
 
-# C
-print("start Q")
-Q = calculate.Q_(excess_return)
-C_dict = dict(zip(result.columns,Q))
-C_rank =  calculate.sort_by_value(C_dict)
-C_last_dict = {}
-C_last_dict["C"]=C_rank
-C_last = pd.DataFrame.from_dict(C_last_dict)
+# # C
+# print("start Q")
+# Q = calculate.Q_(excess_return)
+# C_dict = dict(zip(result.columns,Q))
+# C_rank =  calculate.sort_by_value(C_dict)
+# C_last_dict = {}
+# C_last_dict["C"]=C_rank
+# C_last = pd.DataFrame.from_dict(C_last_dict)
 
-# print(A_dict)
-# print(A_last.head(10))
-# print(B_dict)
-# print(B_last.head(10))
-print(C_dict)
-print(C_last.head(10))
+print(A_dict)
+print(A_last.head(10))
+print(B_dict)
+print(B_last.head(10))
+# print(C_dict)
+# print(C_last.head(10))
